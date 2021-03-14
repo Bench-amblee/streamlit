72901fe3e605b0f4df8f7e801a2a6df7c877791b
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-03-14 18:45:05 -0500

    Update color_palette.py

--- color_palette.py
+++ color_palette.py
@@ -95,7 +95,7 @@ def color_change(hls,hue_val,light_val,sat_val,num_colors,string):
     og = sns.palplot(hex_1[:num_colors])
     st.write(string)
     st.pyplot(og)
-    
+
 image_display(choice)
 palette(hls,1,1,1,6,'original colors')
 color_change(hls,1.5,1,1,6,'green hue colors')
