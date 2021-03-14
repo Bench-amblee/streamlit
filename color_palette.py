827c9741e62cf98b896d8fb4ad28ae69cc0fe96e
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-03-14 18:08:00 -0500

    Update color_palette.py

--- color_palette.py
+++ color_palette.py
@@ -21,7 +21,7 @@ images = {'forest': image1,'sunset':image2,'coffee':image3,'random':random_image
 st.title('Image Palette Generator')
 st.write('select an image below, a color palette based on the most prominent colors will generate below')
 
-choice = st.selectbox('select an image or upload your own (coming soon!',['forest','sunset','coffee','random'])
+choice = st.selectbox('select an image or upload your own (coming soon!)',['forest','sunset','coffee','random'])
 if choice == 'random':
     if st.button('New Image'):
         random_url = random_url[:len(random_url)-1]
