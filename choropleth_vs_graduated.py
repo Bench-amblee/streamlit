ce1e50dc5e1e62842d526579d192e6402c6597cb
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-04-08 22:50:44 -0500

    Update choropleth_vs_graduated.py

--- choropleth_vs_graduated.py
+++ choropleth_vs_graduated.py
@@ -13,18 +13,34 @@ grad2 = Image.open('images/grad2.png')
 
 st. set_page_config(layout="wide")
 st.title('Choropleth Maps vs Graduated Symbols Maps')
-time.sleep(5)
+
+choice = st.selectbox('select a type of map to view',['Choropleth','Graduated Symbol'])
 col1, col2 = st.beta_columns(2)
 
-with col1:
-  st.title('Choropleth Map')
-  chart1 = chor1
-  if st.button('Value = 50'):
-    chart1 = chor2
-  st.image(chart1)
-with col2:
-  st.title('Graduated Symbols Map')
-  chart2 = grad1
-  if st.button('Graduated Value = 50'):
-    chart2 = grad2
-  st.image(chart2)
+if choice == 'Choropleth':
+  with col1:
+    st.title('Choropleth Map')
+    chart1 = chor1
+    if st.button('Value = 50'):
+      chart1 = chor2
+    st.image(chart1)
+  with col2:
+    st.title('Graduated Symbols Map')
+    chart2 = grad1
+    if st.button('Graduated Value = 50'):
+      chart2 = grad2
+    st.image(chart2)
+
+if choice == 'Graduated Symbol':
+  with col1:
+    st.title('Choropleth Map')
+    chart1 = grad1
+    if st.button('Value = 50'):
+      chart1 = grad2
+    st.image(chart1)
+  with col2:
+    st.title('Graduated Symbols Map')
+    chart2 = grad1
+    if st.button('Graduated Value = 50'):
+      chart2 = grad2
+    st.image(chart2)
