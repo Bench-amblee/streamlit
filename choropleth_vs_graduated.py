31df111903d9a411fa63791381a4d6c084d10b2a
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-04-08 23:00:55 -0500

    Update choropleth_vs_graduated.py

--- choropleth_vs_graduated.py
+++ choropleth_vs_graduated.py
@@ -20,17 +20,18 @@ col1, col2 = st.beta_columns(2)
 
 if choice == 'Choropleth':
   with col1:
-    st.title('Choropleth Map')
     chart1 = chor1
     if st.button('Value = 50'):
       chart1 = chor2
     st.image(chart1)
   with col2:
-    st.title('Graduated Symbols Map')
-    chart2 = grad1
-    if st.button('Graduated Value = 50'):
-      chart2 = grad2
-    st.image(chart2)
+    st.title('Choropleth Map)
+    st.write('Choropleth maps are data maps that use differences in shading/coloring within predefined areas to indicate the average values of a property or quantity in those areas.
+    st.write('They are good for:
+    st.write('- displaying large amounts of data across large spatial extents'
+    st.write('- Easily identifying outliers'
+    st.write('A disatvantage of using choropleth maps:
+    st.write('choosing a different boundary, for example county lines vs. state senate districts, could imply completely different spatial relationships')
 
 if choice == 'Graduated Symbol':
   with col1:
